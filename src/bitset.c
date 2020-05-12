bf9d0404cb7c87809d97bdd6e60bfeea3e6add0c
Donovan Allen <me@donovanallen.net>
2020-05-11 22:19:26 -0700

    Add robinhood hashing

--- src/bitset.c
+++ src/bitset.c
@@ -3,6 +3,8 @@
 #include "lib/allocator.h"
 #include "lib/common.h"
 
+#define BLOCK_LEN 8
+
 struct set_struct
 {
     uint8_t *table;
@@ -13,7 +15,7 @@ set_t *newSet(size_t _)
 {
     size_t ipRange = (1L << 32) - 1;
     set_t *set = profiledCalloc(1, sizeof(set_t));
-    set->table = profiledCalloc(ipRange / 8, sizeof(uint8_t));
+    set->table = profiledCalloc(ipRange / BLOCK_LEN, sizeof(uint8_t));
     return set;
 }
 
@@ -27,9 +29,9 @@ void addIP(uint32_t address, void *context)
 {
     set_t *set = (set_t *)context;
 
-    size_t offset = address / 8;
+    size_t offset = address / BLOCK_LEN;
     uint8_t *seg = set->table + offset;
-    size_t mask = 1 << (address % 8);
+    size_t mask = 1 << (address % BLOCK_LEN);
     if ((*seg & mask) == 0)
     {
         set->size++;
@@ -44,9 +46,9 @@ void setAddAll(set_t *set, subnet_t addresses)
 
 bool setContains(set_t *set, uint32_t address)
 {
-    size_t offset = address / 8;
+    size_t offset = address / BLOCK_LEN;
     uint8_t *seg = set->table + offset;
-    size_t mask = 1 << (address % 8);
+    size_t mask = 1 << (address % BLOCK_LEN);
     return (*seg & mask) != 0;
 }
 
---
+++ src/robinhood_hashing.c
@@ -0,0 +1,129 @@
+#include "blacklist.h"
+#include "lib/allocator.h"
+#include "lib/common.h"
+#include <assert.h>
+
+#define EMPTY 0
+#define DELETED 255
+
+struct set_struct
+{
+    uint32_t *table;
+    size_t table_len;
+    size_t table_used;
+    size_t max_dist;
+};
+
+set_t *newSet(size_t len)
+{
+    set_t *set = profiledCalloc(1, sizeof(set_t));
+    set->table_len = len * 1.3;
+    set->table_used = 0;
+    set->table = profiledCalloc(set->table_len, sizeof(uint32_t));
+    set->max_dist = 0;
+
+    return set;
+}
+
+void deleteSet(set_t *set)
+{
+    profiledFree(set->table);
+    profiledFree(set);
+}
+
+// Seems to be pretty good
+// https://github.com/skeeto/hash-prospector
+size_t hash(uint32_t x)
+{
+    x ^= x >> 16;
+    x *= UINT32_C(0x7feb352d);
+    x ^= x >> 15;
+    x *= UINT32_C(0x846ca68b);
+    x ^= x >> 16;
+    return x & 0xffffffff;
+}
+
+size_t displacementOf(set_t *set, size_t loc)
+{
+    return loc - (hash(set->table[loc]) % set->table_len);
+}
+
+void addIP(uint32_t address, void *context)
+{
+    if (address == EMPTY)
+    {
+        return;
+    }
+
+    uint32_t original_address = address;
+
+    set_t *set = (set_t *)context;
+    assert(set->table_used < set->table_len);
+    size_t def_pos;
+    size_t ins_pos = def_pos = hash(address) % set->table_len;
+    for (; set->table[ins_pos] != EMPTY; ins_pos = (ins_pos + 1) % set->table_len)
+    {
+        if (set->table[ins_pos] == original_address)
+        {
+            return;
+        }
+
+        // if the displacement of the inserting element is further than the displacement of the current element
+        // swap the two and continue
+        if (displacementOf(set, ins_pos) < ins_pos - def_pos)
+        {
+            // record the displacement
+            if (ins_pos - def_pos > set->max_dist)
+            {
+                set->max_dist = ins_pos - def_pos;
+            }
+            // swap the two
+            uint32_t x = set->table[ins_pos];
+            set->table[ins_pos] = address;
+            address = x;
+            // reset the counting and such
+            ins_pos = def_pos = hash(address) % set->table_len;
+        }
+    }
+
+    // update max displacement
+    if (ins_pos - def_pos > set->max_dist)
+    {
+        set->max_dist = ins_pos - def_pos;
+    }
+
+    // drop final entry
+    set->table[ins_pos] = address;
+    set->table_used++;
+}
+
+void setAddAll(set_t *set, subnet_t addresses)
+{
+    iterateIPs(&addresses, set, addIP);
+}
+
+bool setContains(set_t *set, uint32_t address)
+{
+    if (address == EMPTY)
+    {
+        return true;
+    }
+
+    int def_pos, data_pos;
+    for (data_pos = def_pos = hash(address) % set->table_len;
+        set->table[data_pos] != EMPTY && set->table[data_pos] != DELETED && (data_pos - def_pos) <= set->max_dist;
+        data_pos = (data_pos + 1) % set->table_len)
+    {
+        if (set->table[data_pos] == address)
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+size_t setGetSize(set_t *set)
+{
+    return set->table_used;
+}
